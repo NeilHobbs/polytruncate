e89c9cda9177663cab9f098021a2290f3b999484
NeilHobbs <neilphobbs@hotmail.com>
2021-05-24 16:20:57 +0100

    to be replaced with single style functions.

--- R/response_fitness_cost_selection.R
+++
@@ -1,8 +0,0 @@
-response_fitness_cost_selection = function(fitness.selection.differential,
-                                             heritability){
-
-  response = heritability * fitness.selection.differential
-
-  return(response)
-
-}
