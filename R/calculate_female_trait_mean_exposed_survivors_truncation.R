084f9c68196fb13af22f9b1a462e527c02a55f69
NeilHobbs <neilphobbs@hotmail.com>
2021-05-26 14:23:55 +0100

    missing brackets

--- R/calculate_female_trait_mean_exposed_survivors_truncation.R
+++ R/calculate_female_trait_mean_exposed_survivors_truncation.R
@@ -4,8 +4,9 @@
 #'@param female.trait.mean = The mean Polygenic Resistance Score to insecticide i of female mosquitoes before selection has occurred.
 
 calculate_female_trait_mean_exposed_survivors_truncation = function(female.exposed.selection.differential,
-                                                                    female.trait.mean)
+                                                                    female.trait.mean){
 
   female.trait.mean.exposed.survivors = female.exposed.selection.differential + female.trait.mean
 
 return(female.trait.mean.exposed.survivors)
+}
