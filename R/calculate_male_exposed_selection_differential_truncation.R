f2566f5d2a400c29addd48025e5b9f7f8cca00d8
Neil Hobbs <neilphobbs@hotmail.com>
2021-07-09 11:20:17 +0100

    needs unit test

---
+++ R/calculate_male_exposed_selection_differential_truncation.R
@@ -0,0 +1,16 @@
+#'@title Calculate the selection differential between male mosquitoes that were exposed and unexposed to the insecticide assuming truncation selection.
+#'
+#'@param field.survival = The survival probability in the field to the insecticide.
+#'@param standard.deviation = The standard deviation of the trait mean in the population.
+
+
+
+calculate_male_exposed_selection_differential_truncation = function(field.survival,
+                                                                    standard.deviation){
+
+
+  male.exposed.selection.differential  = standard.deviation * (dnorm(qnorm(1-field.survival))/field.survival)
+
+  return(male.exposed.selection.differential)
+
+}
