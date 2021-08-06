a7c10efc0b1fab38aa86ae643586e9301c6fbca9
NeilHobbs <neilphobbs@hotmail.com>
2021-08-06 12:59:09 +0100

    minor corrections

--- R/wrapper_calculate_population_suppresion.R
+++ R/wrapper_calculate_population_suppresion.R
@@ -29,41 +29,39 @@ relative.contributions.before.selection = calculate_density_of_trait_values(vect
                                                                             standard.deviation = standard.deviation)
 
 total.female.population.size = get_female_total_population_size(
-  total.population.size = get_total_population_size(
+  total.population.size = get_total_population_size_truncation(
     relative.contributions.before.selection = relative.contributions.before.selection))
 
 ##Get the female population size that is unexposed to the insecticide:
-
-female.population.size.unexposed = calculate_female_population_size_unexposed(total.female.population.size = total.female.population.size,
+female.population.size.unexposed = calculate_female_population_size_unexposed_truncation(total.female.population.size = total.female.population.size,
                                                                               female.insecticide.exposure = female.insecticide.exposure)
 
 ##Get the female population size are the exposed survivors:
-relative.female.contributions.after.selection = calculate_female_density_after_selection(
-  female.insecticide.exposure = female.insecticide.exposure,
-  vector.length = vector.length,
-  trait.mean = trait.mean.deployed,
-  standard.deviation = standard.deviation,
-  maximum.bioassay.survival.proportion = maximum.bioassay.survival.proportion,
-  michaelis.menten.slope = michaelis.menten.slope,
-  half.population.bioassay.survival.resistance = half.population.bioassay.survival.resistance,
-  regression.coefficient = regression.coefficient,
-  regression.intercept = regression.intercept,
-  current.insecticide.efficacy = current.insecticide.efficacy
-)
+exposed.female.mosquito.population = total.female.population.size - female.population.size.unexposed
 
-female.population.size.exposed.survivors = calculate_female_population_size_exposed_survivors(
-  relative.female.contributions.after.selection = relative.female.contributions.after.selection)
+field.survival = convert_bioassay_survival_to_field_survival(bioassay.survival = convert_resistance_score_to_bioassay_survival(maximum.bioassay.survival.proportion = maximum.bioassay.survival.proportion,
+                                                                                                                                          trait.mean = trait.mean.deployed,
+                                                                                                                                          michaelis.menten.slope = michaelis.menten.slope,
+                                                                                                                                          half.population.bioassay.survival.resistance = half.population.bioassay.survival.resistance),
+                                                             regression.coefficient = regression.coefficient,
+                                                             regression.intercept = regression.intercept,
+                                                             current.insecticide.efficacy = current.insecticide.efficacy)
 
 
-#Then obtain the final relative population size in the intervention site (for females only):
-  #This is obtained by unexposed + exposed.survivors
-female.population.size.after.selection = calculate_female_population_size_after_selection(female.population.size.unexposed = female.population.size.unexposed,
-                                                                                          female.population.size.exposed.survivors = female.population.size.exposed.survivors)
+
+
+exposed.female.mosquito.population.survivors = exposed.female.mosquito.population * field.survival
+
+
+end.female.population.size = exposed.female.mosquito.population.survivors + female.population.size.unexposed
+
+
+
 
 
 
 #Finally find the proportion of the population in the intervention site that survives.
-proportion.surviving =  calculate_insecticide_population_suppression(female.population.size.after.selection = female.population.size.after.selection,
+proportion.surviving =  calculate_insecticide_population_suppression(female.population.size.after.selection = end.female.population.size,
                                                                                   total.female.population.size = total.female.population.size)
 
 #insecticide.population.suppression = 1 - proportion.surviving
